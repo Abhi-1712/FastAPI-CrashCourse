cf3aa7c5839176b3ddf41a52da524f39d04dbaa3
Abhijeet Ingle <abhijeet171202@gmail.com>
2023-02-16 15:57:59 +0530

    Get Route Done

--- fastapi/app/app.py
+++ fastapi/app/app.py
@@ -1,6 +1,32 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+
+# minimal app - get request
 @app.get("/",tags=['ROOT'])
 async def root() -> dict:
     return{'Ping':'Pong'}
+
+
+# Get --> Read Todo
+@app.get('/todo',tags=['todos'])
+async def get_todo() -> dict:
+    return{"data": todos}
+
+
+todos = [
+    {
+        "id":"1",
+        "Activity":"Jogging for 2 hours at 7:00 AM."
+    },
+    {
+        "id":"2",
+        "Activity":"Writing 3 pages of my new book at 2:00 PM."
+    }
+]
+
+
+
+# Post --> Create Todo
+# Put --> Update Todo
+# Delete --> Delete Todo
